698dff675a6ade7bd474d9ce2c35af2e1befecab
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-06-12 19:06:55 +0300

    use plotly for graphs

--- data_plotter.py
+++ data_plotter.py
@@ -1,8 +1,5 @@
-
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-from mpl_finance import candlestick_ochl
+import plotly as plt
 
 from data_retrieve import QUANDLDATA
 
@@ -10,28 +7,21 @@ from data_retrieve import QUANDLDATA
 class Plot(object):
     data = []
 
-    def __init__(self, fig):
-        self.ax1 = fig.add_subplot(1, 1, 1)
-
     def __clean_data(self, request):
         data = request.get("dataset_data").get("data")
         headers = request.get("dataset_data").get("column_names")
         clean_data = pd.DataFrame(data, None, list(
             map(lambda x: x.lower(), headers)))
         clean_data.date = pd.to_datetime(clean_data.date, format="%Y-%M-%d")
-        clean_data.date = pd.to_numeric(clean_data.date)
         return clean_data
 
     def plot(self):
         q = QUANDLDATA()
-        data = q.get_data()
+        data = q.get_data('2015-05-24', '2015-05-28')
         clean_data = self.__clean_data(data)
-        candlestick_ochl(self.ax1, clean_data.values,
-                         width=0.4,
-                         colorup='#77d879', colordown='#db3f3f')
-        self.ax1.clear()
-        self.ax1.plot(clean_data)
-        plt.xlabel('Date')
-        plt.ylabel('Price')
-        plt.title('Live graph with matplotlib')
+        trace = go.Ohlc(x=clean_data.date, open=clean_data.open,
+                        close=clean_data.close, high=clean_data.high,
+                        low=clean_data.low, name="Currencies")
+        data = [trace]
+        plt.offline.plot(data, filename="text.html")
         return plt
--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-# %%
 from data_plotter import Plot
-import matplotlib.animation as animation
-import matplotlib.pyplot as plt
-
 
 if __name__ == "__main__":
-    fig = plt.figure()
-    graph = Plot(fig)
-    plot = graph.plot()
-    plot.show()
+    graph = Plot()
+    graph.plot()
 
-
-# %%
